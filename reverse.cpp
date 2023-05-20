2464f3edd1095c217d8dbfb5e632d306cc2dfdda
Avinash kumar pandey <132198564+Avinash7084@users.noreply.github.com>
2023-05-20 19:09:30 +0700

    Create reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    int num1;
+    
+    cout<<" Enter a first number: ";
+    cin>>num1;
+    
+    
+    for(i=num1; i>=1; i--){
+    
+    cout<<i<<endl;
+    }
+    return 0;
+}
